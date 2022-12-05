281a9a4c3310d2b44ea6669bf2e321366d56c8a5
doctordehi <doctordehi@gmail.com>
2022-12-05 14:09:24 +0100

    soubory

--- generating_pedigree/doc_gen_test.py
+++ generating_pedigree/doc_gen_test.py
@@ -3,7 +3,7 @@ from docx.shared import Inches
 
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("Rodokmen_holuba.docx")
+doc = DocxTemplate("../Rodokmen_holuba.docx")
 context = { 'company_name' : "World company" }
 doc.render(context)
 doc.save("generated_doc.docx")
